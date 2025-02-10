15f3f18784fcc00fb0d7780286bfcbf5448d87c8
Tim Howe <tbhowe@gmail.com>
2025-02-10 19:42:04 +0000

    wraps tidal coeff calc into a class

--- src/api_calls/tidal_coeffiicent_test.py
+++ src/api_calls/tidal_coeffiicent_test.py
@@ -1,43 +1,32 @@
-#%%
+# api_calls/tidal_coefficient.py
 from skyfield.api import Topos, load
 from datetime import datetime
 
-def compute_declinations(ts, lat, lon, date):
-    """Compute the solar and lunar declinations for a given date and location."""
-    
-    planets = load('de421.bsp')  # Loading planetary ephemeris data
-    earth = planets['earth']
-    sun = planets['sun']
-    moon = planets['moon']
-    
-    observer = earth + Topos(latitude_degrees=lat, longitude_degrees=lon)
-    
-    t = ts.utc(date.year, date.month, date.day)
-    
-    astrometric_sun = observer.at(t).observe(sun).apparent()
-    astrometric_moon = observer.at(t).observe(moon).apparent()
-    
-    alt_sun, az_sun, d_sun = astrometric_sun.altaz()
-    alt_moon, az_moon, d_moon = astrometric_moon.altaz()
-    
-    return alt_sun.degrees, alt_moon.degrees
+class TidalCoefficient:
+    def __init__(self):
+        # Preload ephemeris data and timescale for reuse.
+        self.planets = load('de421.bsp')
+        self.earth = self.planets['earth']
+        self.sun = self.planets['sun']
+        self.moon = self.planets['moon']
+        self.timescale = load.timescale()
 
-def tidal_coefficient(lat, lon, date):
-    """Compute tidal coefficient based on location and date."""
-    
-    ts = load.timescale()  # Loading timescale data for date calculations
-    
-    declination_sun, declination_moon = compute_declinations(ts, lat, lon, date)
-    
-    # Formula for tidal coefficient based on declinations might vary, adjust as needed
-    coefficient = abs(declination_sun) + abs(declination_moon)
-    
-    return coefficient
+    def compute_declinations(self, lat, lon, date):
+        """Compute the solar and lunar altitudes (used here as a proxy for declination) for a given date and location."""
+        observer = self.earth + Topos(latitude_degrees=lat, longitude_degrees=lon)
+        t = self.timescale.utc(date.year, date.month, date.day)
+        astrometric_sun = observer.at(t).observe(self.sun).apparent()
+        astrometric_moon = observer.at(t).observe(self.moon).apparent()
+        alt_sun, _, _ = astrometric_sun.altaz()
+        alt_moon, _, _ = astrometric_moon.altaz()
+        return alt_sun.degrees, alt_moon.degrees
 
-if __name__ == "__main__":
-    date_obj = datetime.strptime("2023-09-16", "%Y-%m-%d")
-    lat, lon = 50.220564, -4.801677
-    coef = tidal_coefficient(lat, lon, date_obj)
-    print(f'Tidal Coefficient for {date_obj.date()}: {coef}')
-
-# %%
+    def get_tidal_coefficient(self, lat, lon, date):
+        """
+        Compute tidal coefficient based on location and date.
+        For our purposes, we use a simple formula:
+           tidal_coefficient = |declination_sun| + |declination_moon|
+        """
+        declination_sun, declination_moon = self.compute_declinations(lat, lon, date)
+        coefficient = abs(declination_sun) + abs(declination_moon)
+        return coefficient
